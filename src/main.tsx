d7cbefbbad831c11d14e935d6fd6a31e1e18e61b
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-02-16 17:51:17 +0100

    Add MUI theme and baseline-css-reset, remove global css files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
+// import './index.css'
+import {createTheme, ThemeProvider, CssBaseline} from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000', // black
+        },
+        secondary: {
+            main: '#808080', // gray
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+);
+
