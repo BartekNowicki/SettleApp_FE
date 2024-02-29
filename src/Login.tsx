1083506793847d16450102fd63b03c3c454dc055
Bartosz Nowicki <bartosznowickihq@gmail.com>
2024-02-29 18:05:49 +0100

    Add login route and component, fix error

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,8 +1,40 @@
+import React from 'react';
+import {Box, Button, Container, TextField} from '@mui/material';
+
 const Login = () => {
+
+    const handleClick = () => console.log("CLICK");
+
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <Container maxWidth="sm">
+            <Box sx={{mt: 8, textAlign: 'center'}}>
+                {/*<Typography variant="h4" gutterBottom></Typography>*/}
+                <Box sx={{mt: 3}}>
+                    <TextField
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                    />
+                </Box>
+                <Box sx={{mt: 2}}>
+                    <TextField
+                        fullWidth
+                        id="password"
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        margin="normal"
+                    />
+                </Box>
+                <Box sx={{mt: 2}}>
+                    <Button variant="contained" color="primary" onClick={() => handleClick()}>
+                        Sign In
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,79 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
-import App from './App';
-import {Box, Container, createTheme, CssBaseline, ThemeProvider} from '@mui/material';
-import {store} from "./store/store.ts";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Box, Container, createTheme, CssBaseline, ThemeProvider, Button } from '@mui/material';
+import { store } from "./store/store.ts";
+import Login from "./Login.tsx";
+import App from "./App";
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#000000',
+            main: '#151315',
         },
         secondary: {
-            main: '#0c0c0c',
+            main: '#1e1d1e',
         },
         background: {
-            default: '#171717',
-            paper: '#1c1c1c',
-        }
+            default: '#000000',
+            paper: '#000000',
+        },
+        success: {
+            main: '#4caf50',
+        },
+        text: {
+            primary: '#ffffff',
+        },
     },
     components: {
         MuiCssBaseline: {
             styleOverrides: {
                 body: {
-                    backgroundColor: '#0a0a0a',
+                    backgroundColor: '#000000',
                 },
             },
         },
     },
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <Container maxWidth="lg" sx={{
-                    minHeight: '100vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: 'background.paper'
-                }}>
-                    <Box sx={{
-                        height: '720px',
-                        width: '80%',
-                        border: '1px solid red'
+            <Router>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Container maxWidth="lg" sx={{
+                        minHeight: '100vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: 'background.paper'
                     }}>
-                        <App/>
-                    </Box>
-                </Container>
-            </ThemeProvider>
+                        <Box sx={{
+                            height: '720px',
+                            width: '80%',
+                            border: '1px solid gray',
+                            background: 'linear-gradient(#6e6e6e, #6e6e6e, #6e6e6e, #6e6e6e)',
+                            padding: '20px',
+                            borderRadius: '10px',
+                            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',
+                        }}>
+                            <Routes>
+                                <Route path="/" element={<App />} />
+                                <Route path="login" element={<Login />} />
+                                <Route path="*" element={<Navigate to="/" />} />
+                            </Routes>
+                            {/*<Button variant="contained" sx={{ mt: 2, color: 'text.primary', bgcolor: 'success.main' }}>*/}
+                            {/*    Success Button*/}
+                            {/*</Button>*/}
+                        </Box>
+                    </Container>
+                </ThemeProvider>
+            </Router>
         </Provider>
     </React.StrictMode>,
 );
